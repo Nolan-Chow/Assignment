4cad09563aadc3873474876980f7f7c60d9d32a1
Nolan <nolan.chow446@gmail.com>
2020-11-28 18:23:55 -0800

    save button functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-function darktheme (e) {
+function darktheme(e) {
     let main = document.querySelector('main');
     let nav = document.querySelector('nav');
     let dark = [main, nav, e.target];
@@ -9,7 +9,7 @@ function darktheme (e) {
     e.target.removeEventListener('click', darktheme);
     e.target.addEventListener('click', lighttheme);
 }
-function lighttheme (e) {
+function lighttheme(e) {
     let main = document.querySelector('main');
     let nav = document.querySelector('nav');
     main.id = 'lightmode_main';
@@ -19,30 +19,67 @@ function lighttheme (e) {
     e.target.removeEventListener('click', lighttheme);
     e.target.addEventListener('click', darktheme);
 }
-function remove (e) {
+let theme = document.querySelector('.grey');
+theme.addEventListener('click', darktheme);
+
+function remove(e) {
     let new_note = document.querySelector('.row1 .green');
     let note = document.querySelector('textarea');
     let save = document.querySelector('.row3 .green');
-    let dark = document.querySelector('.grey');
-    let disappear = [note, save, dark, e.target];
+    let disappear = [note, save, e.target];
     for (let element of disappear) {
         element.style.display = 'none';
     }
     new_note.addEventListener('click', add);
 }
-function add (e) {
+function add(e) {
     let note = document.querySelector('textarea');
     let save = document.querySelector('.row3 .green');
-    let dark = document.querySelector('.grey');
     let cancel = document.querySelector('.row3 .red');
-    let appear = [note, save, dark, cancel];
+    let appear = [note, save, cancel];
     for (let element of appear) {
         element.toggleAttribute('style')
     }
-    note.value = '  ';
+    note.value = '';
     e.target.removeEventListener('click', add)
 }
-let theme = document.querySelector('.grey');
-theme.addEventListener('click', darktheme);
 let cancel = document.querySelector('.row3 .red')
 cancel.addEventListener('click', remove);
+
+function store_notes() {
+    let notes = document.querySelector('textarea').value;
+    let lines = notes.split('\n');
+    let first_line = lines[0];
+    if (lines.length === 1 && first_line !== '') {
+        notesArray.push({
+                title: first_line,
+                body: ''
+            });
+        addtolist(first_line);
+    } else if (lines.length > 0 && first_line !== '') {
+        lines.shift()
+        let value = lines.join('\n')
+        notesArray.push({
+            title: first_line,
+            body: value
+        });
+        addtolist(first_line);
+    }
+    document.querySelector('textarea').value = ''
+}
+function addtolist(title) {
+    list = document.querySelector('nav ul');
+    listitem = document.createElement('li');
+    listitem.innerText = title;
+    list.appendChild(listitem);
+}
+let notesArray = [{
+        title: 'note one',
+        body: 'some text 1'
+    },
+    {
+        title: 'note two',
+        body: 'some text 2'
+    }];
+let save = document.querySelector('.row3 .green');
+save.addEventListener('click', store_notes);
