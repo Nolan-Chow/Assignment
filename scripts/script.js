92feadc0f8a92d8879f2b6e7730066548e0bf848
Nolan <nolan.chow446@gmail.com>
2020-11-28 16:23:36 -0800

    new note and cancel button functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
 function darktheme (e) {
     let main = document.querySelector('main');
     let nav = document.querySelector('nav');
-    main.toggleAttribute("id");
-    nav.toggleAttribute("id");
-    e.target.toggleAttribute("id");
+    let dark = [main, nav, e.target];
+    for (let element of dark) {
+        element.toggleAttribute('id');
+    }
     e.target.innerText = 'Light Theme';
     e.target.removeEventListener('click', darktheme);
     e.target.addEventListener('click', lighttheme);
@@ -18,5 +19,30 @@ function lighttheme (e) {
     e.target.removeEventListener('click', lighttheme);
     e.target.addEventListener('click', darktheme);
 }
-let btn = document.querySelector('.grey');
-btn.addEventListener('click', darktheme);
+function remove (e) {
+    let new_note = document.querySelector('.row1 .green');
+    let note = document.querySelector('textarea');
+    let save = document.querySelector('.row3 .green');
+    let dark = document.querySelector('.grey');
+    let disappear = [note, save, dark, e.target];
+    for (let element of disappear) {
+        element.style.display = 'none';
+    }
+    new_note.addEventListener('click', add);
+}
+function add (e) {
+    let note = document.querySelector('textarea');
+    let save = document.querySelector('.row3 .green');
+    let dark = document.querySelector('.grey');
+    let cancel = document.querySelector('.row3 .red');
+    let appear = [note, save, dark, cancel];
+    for (let element of appear) {
+        element.toggleAttribute('style')
+    }
+    note.value = '  ';
+    e.target.removeEventListener('click', add)
+}
+let theme = document.querySelector('.grey');
+theme.addEventListener('click', darktheme);
+let cancel = document.querySelector('.row3 .red')
+cancel.addEventListener('click', remove);
